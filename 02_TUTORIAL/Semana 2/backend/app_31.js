3c47c763b71d3d9e00bc757ce76cbbe8819f087f
Giovana Thomé <giovana.thome@sou.inteli.edu.br>
2022-05-05 15:39:10 -0300

    Atualização do currículo web com CSS
    CSS adicionado ao currículo WEB
    Caminho da pasta: repositorioGiovanaThome\03_AUT_EST_ENTREGA\Semana 3\curriculo_web_com_css

--- 02_TUTORIAL/Semana 2/backend/app_31.js
+++ 02_TUTORIAL/Semana 2/backend/app_31.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3022;
+const port = 3031;
 app.use(express.static("../frontend/"));
 
 app.listen(port, hostname, () => {

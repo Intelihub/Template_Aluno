2f0c372821cab89bb53939d9cb715f891b95e8cd
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-05-10 20:01:30 -0300

    atualizacao

---
+++ 02_TUTORIAL/Semana 4/frontend/EX1/script.js
@@ -0,0 +1,90 @@
+﻿
+
+var ModuloSkynet = { 
+    avaliacaoQI: Number(0), // Avaliacao de inteligencia calculada
+    perguntaTeste: "A ser inserida",
+    respostas: ["A - qq coisa", "B - qq coisa"],
+    correta: 0,
+    respondida: 0,
+    atualizaQI: function(){
+        this.avaliacaoQI += Number(20);
+    }
+};
+
+var DisciplinaHogwarts = {
+    nomeDisciplina: "",
+    notasAlunos: [],
+    pesoGrif: 1,
+    pesoSli: 1,
+    pesoHufle: 1,
+    pesoRaven: 1,
+    mediaAlunos: function(){
+        return 1;
+    },
+    
+
+
+};
+
+var convertidoJSON = "";
+var ObjDeJSON;
+
+$(document).ready( function() {
+    console.log("aguardando");
+
+    $("#IDgeraJSON").click(function (evento){
+            console.log("Iniciando conversao:");
+            evento.preventDefault();
+            convertidoJSON = JSON.stringify(ModuloSkynet);
+            $("#IDtoJSON").append(convertidoJSON); 
+         });
+    $("#IDexecutaOBJ").click(function (evento){
+            console.log("Altera o QI :");
+            evento.preventDefault();
+            ModuloSkynet.atualizaQI();
+            convertidoJSON = JSON.stringify(ModuloSkynet);
+            $("#IDtoJSON2").append(convertidoJSON); 
+    });
+
+    $("#IDmexeJSON").click(function (evento){
+            console.log("Sera que o JSON agora faz algo ? :");
+            evento.preventDefault();
+            convertidoJSON = JSON.stringify(ModuloSkynet);
+            try{
+                convertidoJSON.avaliacaoQI = 100;
+                convertidoJSON = JSON.stringify(ModuloSkynet);
+                $("#IDtoJSON3").append(convertidoJSON); 
+            }catch(ex){
+                $("#IDtoJSON3").append("ERRO: Sinto muito... não vai rolar não </br>");
+                $("#IDtoJSON3").append("ERRO:" + ex);
+            }
+           
+
+    });
+
+
+    $("#IDmexeJSON2").click(function (evento){
+        console.log("Sera que o JSON agora faz algo ? :");
+        evento.preventDefault();
+        convertidoJSON = JSON.stringify(ModuloSkynet);
+        try{
+            convertidoJSON = JSON.stringify(ModuloSkynet);
+            convertidoJSON.atualizaQI()
+            $("#IDtoJSON31").append(convertidoJSON); 
+        }catch(ex){
+            $("#IDtoJSON31").append("ERRO: Sinto muito... não vai rolar não </br>");
+            $("#IDtoJSON31").append("ERRO:" + ex);
+        }
+       
+
+});
+
+    $("#IDleJSON").click(function (evento){
+        evento.preventDefault();
+        ModuloSkynet.atualizaQI();
+        ObjDeJSON = JSON.parse($("#IDJSONtoConvert").val());
+        $("#IDtoJSON4").append(JSON.stringify(ObjDeJSON)); 
+    });
+});
+
+

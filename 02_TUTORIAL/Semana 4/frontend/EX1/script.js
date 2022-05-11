f460cfde3a6d1c063eb5f636d39c0f560c64bd05
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-05-10 23:43:00 -0300

    UPDADE

--- 02_TUTORIAL/Semana 4/frontend/EX1/script.js
+++ 02_TUTORIAL/Semana 4/frontend/EX1/script.js
@@ -1,90 +1,91 @@
 ﻿
-
-var ModuloSkynet = { 
-    avaliacaoQI: Number(0), // Avaliacao de inteligencia calculada
-    perguntaTeste: "A ser inserida",
-    respostas: ["A - qq coisa", "B - qq coisa"],
-    correta: 0,
-    respondida: 0,
-    atualizaQI: function(){
-        this.avaliacaoQI += Number(20);
-    }
-};
-
 var DisciplinaHogwarts = {
     nomeDisciplina: "",
     notasAlunos: [],
     pesoGrif: 1,
     pesoSli: 1,
-    pesoHufle: 1,
     pesoRaven: 1,
+    pesoHufle: 1,    
     mediaAlunos: function(){
+        // varre a lista de notas dos alunos e retorna a media
         return 1;
     },
-    
-
-
+    mediaCasa: function(nrcasa){
+        // multiplica a media dos alunos vezes o peso da casa e retorna  
+        return 1;      
+    },
+    geraNotas: function(nrnotas){
+        // gera uma lista de notas com valores aleatorios e poe em notasAlunos
+        this.notasAlunos.push(1);
+    } 
 };
+var CasaHogwarts = {
+    nomeCasa: "",
+    numeroCasa: 0,
+    mediaDisciplinas: 1,
+    listaDisciplinas: [],
+    adicionaDisciplina: function(disciplina){
+        // Adiciona Disciplina na lista de disciplinas
+    },
+    calculaMediaDisciplinas: function(){
+        // Varre a lista de disciplinas e tira as medias correspondentes a casa atual
+        // e coloca na variavel "mediaDisciplinas"
+    }
+};
+var listaTodasDisciplinas = [];
+var grifinoria = Object.create(CasaHogwarts);
+var sonserina  = Object.create(CasaHogwarts);
+var corvinal   = Object.create(CasaHogwarts);
+var lufalufa   = Object.create(CasaHogwarts);
 
 var convertidoJSON = "";
 var ObjDeJSON;
 
 $(document).ready( function() {
-    console.log("aguardando");
 
-    $("#IDgeraJSON").click(function (evento){
-            console.log("Iniciando conversao:");
-            evento.preventDefault();
-            convertidoJSON = JSON.stringify(ModuloSkynet);
-            $("#IDtoJSON").append(convertidoJSON); 
-         });
-    $("#IDexecutaOBJ").click(function (evento){
-            console.log("Altera o QI :");
-            evento.preventDefault();
-            ModuloSkynet.atualizaQI();
-            convertidoJSON = JSON.stringify(ModuloSkynet);
-            $("#IDtoJSON2").append(convertidoJSON); 
-    });
+    $("#IDNovaDisciplina").click(function (evento){
+        var nome = $("#IDnomeDis").val();
+        var pg   = $("#IDpesoGrif").val();
+        var ps   = $("#IDpesoSon").val();
+        var pc   = $("#IDpesoCorv").val();
+        var pl   = $("#IDpesoLuf").val();
+        var disciplina = Object.create(DisciplinaHogwarts);
+        disciplina.nomeDisciplina = nome;
+        disciplina.pesoGrif = pg;
+        disciplina.pesoSli  = ps;
+        disciplina.pesoRaven = pc;
+        disciplina.pesoHufle = pl;
+        disciplina.geraNotas();
+        listaTodasDisciplinas.push(disciplina);
 
-    $("#IDmexeJSON").click(function (evento){
-            console.log("Sera que o JSON agora faz algo ? :");
-            evento.preventDefault();
-            convertidoJSON = JSON.stringify(ModuloSkynet);
-            try{
-                convertidoJSON.avaliacaoQI = 100;
-                convertidoJSON = JSON.stringify(ModuloSkynet);
-                $("#IDtoJSON3").append(convertidoJSON); 
-            }catch(ex){
-                $("#IDtoJSON3").append("ERRO: Sinto muito... não vai rolar não </br>");
-                $("#IDtoJSON3").append("ERRO:" + ex);
-            }
-           
+        grifinoria.adicionaDisciplina(disciplina);
+        sonserina.adicionaDisciplina(disciplina);
+        corvinal.adicionaDisciplina(disciplina);
+        lufalufa.adicionaDisciplina(disciplina);
+
+     });
+
+    $("#IDcalculaMedias").click(function (evento){
 
     });
 
+    $("#IDgeraJSONCasas").click(function (evento){
+        var JSONcasa1 = JSON.stringify(grifinoria);
+        $("#IDtoJSON1").append(JSONcasa1); 
 
-    $("#IDmexeJSON2").click(function (evento){
-        console.log("Sera que o JSON agora faz algo ? :");
-        evento.preventDefault();
-        convertidoJSON = JSON.stringify(ModuloSkynet);
-        try{
-            convertidoJSON = JSON.stringify(ModuloSkynet);
-            convertidoJSON.atualizaQI()
-            $("#IDtoJSON31").append(convertidoJSON); 
-        }catch(ex){
-            $("#IDtoJSON31").append("ERRO: Sinto muito... não vai rolar não </br>");
-            $("#IDtoJSON31").append("ERRO:" + ex);
-        }
-       
+        var JSONcasa2 = JSON.stringify(sonserina);
+        $("#IDtoJSON2").append(JSONcasa2); 
+
+        var JSONcasa3 = JSON.stringify(corvinal);
+        $("#IDtoJSON3").append(JSONcasa3); 
+        
+        var JSONcasa4 = JSON.stringify(lufalufa);
+        $("#IDtoJSON4").append(JSONcasa4); 
+    });
 
 });
 
-    $("#IDleJSON").click(function (evento){
-        evento.preventDefault();
-        ModuloSkynet.atualizaQI();
-        ObjDeJSON = JSON.parse($("#IDJSONtoConvert").val());
-        $("#IDtoJSON4").append(JSON.stringify(ObjDeJSON)); 
-    });
-});
+
+
 
 

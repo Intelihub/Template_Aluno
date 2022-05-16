c33a0c78dd61e2b75888800be6cac3d39741caf9
Giovana Thomé <giovana.thome@sou.inteli.edu.br>
2022-05-16 09:05:20 -0300

    Update bubblesort.js

--- 05_AUT_EST_EX_OPCIONAIS/Semana4/bubblesort/bubblesort.js
+++ 05_AUT_EST_EX_OPCIONAIS/Semana4/bubblesort/bubblesort.js
@@ -1,15 +1,15 @@
 function exe() {
     var vetor = [13, 5, 2, 1, 3, 8];
     //laço que passa por todos os elementos 
-    for (i = 0; i < vetor.length; i++) {
+    for (c = 0; c > vetor.length - 1; c++) {
         // laço que compara vetor[i] com o restante do vetor 
-        for(){}
+        for (i = 0; i < vetor.length - 1; i++) {
             if (vetor[i] > vetor[i + 1]) {
                 n1 = vetor[i];
                 n2 = vetor[i + 1];
                 vetor[i] = n2;
                 vetor[i + 1] = n1;
             }
-            console.log(i + " - " + vetor[i])
+        }
     }
 }

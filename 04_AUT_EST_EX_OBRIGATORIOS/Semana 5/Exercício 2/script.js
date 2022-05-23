8d323f107faa9a7f15d8e507c7b0579f57a38ca2
IgorSFG <igor.garcia@sou.inteli.edu.br>
2022-05-23 16:22:36 -0300

    Exercícios Obrigatórios

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercício 2/script.js
@@ -0,0 +1,52 @@
+﻿var vp = 0;
+var nota100 = 0;
+var nota50 = 0;
+var nota20 = 0;
+var nota10 = 0;
+var nota5 = 0;
+var nota2 = 0;
+var nota1 = 0;
+
+function CalcNotas(vp){
+    var v0 = document.getElementById("value").value;
+    if (v0 != 0) {
+        vp = v0;
+    };
+
+    while (vp >= 100) {
+        parseInt(vp) -= 100;
+        nota100 += 1;
+    }
+    while (vp >= 50) {
+        parseInt(vp) -= 50;
+        nota50 += 1;
+    }
+    while (vp >= 20) {
+        parseInt(vp) -= 20;
+        nota20 += 1;
+    }
+    while (vp >= 10) {
+        parseInt(vp) -= 10;
+        nota10 += 1;
+    }
+    while (vp >= 5) {
+        parseInt(vp) -= 5;
+        nota5 += 1;
+    }
+    while (vp >= 2) {
+        parseInt(vp) -= 2;
+        nota2 += 1;
+    }
+    while (vp >= 1) {
+        parseInt(vp) -= 1;
+        nota1 += 1;
+    }
+    if (vp = 0) {
+        ShowNotas(nota100, nota50, nota20, nota10, nota5, nota2, nota1);
+    }
+}
+
+function ShowNotas(vp, nota100, nota50, nota20, nota10, nota5, nota2, nota1){
+    document.getElementById("notas").innerHTML += "<br />"+nota100+" notas de R$100 "+nota50+" notas de R$50 "+nota20+" notas de R$20 "+nota10+" notas de R$10 "+nota5+" notas de R$5 "+nota2+" notas de R$2 "+nota1+" notas de R$1";
+    vp = 0; nota100 = 0; nota50 = 0; nota20 = 0; nota10 = 0; nota5 = 0; nota2 = 0; nota1 = 0;
+}
---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercício 3/script.js
@@ -0,0 +1,94 @@
+﻿var v1 = 0;
+var v2 = 0;
+
+function somar(){
+    var v0 = document.getElementById("value1").value;
+    if (v0 != 0) {
+        v1 = v0;
+    };
+    var v02 = document.getElementById("value2").value
+    if (v02 != 0) {
+        v2 = v02;
+    };
+
+    var op = "+";
+    var result = parseFloat(v1) + parseFloat(v2);
+    ShowOp(v1, v2, op);
+    ShowResult(result);
+}
+
+function subtrair(){
+    var v0 = document.getElementById("value1").value;
+    if (v0 != 0) {
+        v1 = v0;
+    };
+    var v02 = document.getElementById("value2").value
+    if (v02 != 0) {
+        v2 = v02;
+    };
+
+    var op = "-";
+    var result = parseFloat(v1) - parseFloat(v2);
+    ShowOp(v1, v2, op);
+    ShowResult(result);
+}
+
+function multiplicar(){
+    var v0 = document.getElementById("value1").value;
+    if (v0 != 0) {
+        v1 = v0;
+    };
+    var v02 = document.getElementById("value2").value
+    if (v02 != 0) {
+        v2 = v02;
+    };
+
+    var op = "*";
+    var result = parseFloat(v1) * parseFloat(v2);
+    ShowOp(v1, v2, op);
+    ShowResult(result);
+}
+
+function dividir(){
+    var v0 = document.getElementById("value1").value;
+    if (v0 != 0) {
+        v1 = v0;
+    };
+    var v02 = document.getElementById("value2").value
+    if (v02 != 0) {
+        v2 = v02;
+    };
+
+    var op = "/";
+    var result = parseFloat(v1) / parseFloat(v2);
+    ShowOp(v1, v2, op);
+    ShowResult(result);
+}
+
+function dividiresto(){
+    var v0 = document.getElementById("value1").value;
+    if (v0 != 0) {
+        v1 = v0;
+    };
+    var v02 = document.getElementById("value2").value
+    if (v02 != 0) {
+        v2 = v02;
+    };
+
+    var op = "%";
+    var result = parseFloat(v1) % parseFloat(v2);
+    ShowOp(v1, v2, op);
+    ShowResult(result);
+}
+
+/* Função que mostra a operação realizada */
+function ShowOp(v1, v2, op){
+    document.getElementById("calc").innerHTML += "<br />"+v1+""+op+""+v2+" = ";
+}
+
+
+/* Função que mostra o resultado obtido */
+function ShowResult(result){
+    document.getElementById("calc").innerHTML += result;
+    v0 = 0
+}

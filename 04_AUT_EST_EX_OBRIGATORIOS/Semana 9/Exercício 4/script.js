34a00541a90a43a20a263aed4bd5f3f6936c8d72
IgorSFG <igor.garcia@sou.inteli.edu.br>
2022-06-16 18:46:35 -0300

    EX_OBG_SEMANA_09

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 4/script.js
@@ -0,0 +1,22 @@
+﻿function Fibonacci(){
+    //Pega o valor da entrada como número
+    var number = Number(document.getElementById("number").value);
+    //Cria uma variável de valor inícial igual a 1
+    var j = parseInt(1);
+    //Cria uma lista
+    var fibonacci = [];
+    
+    //Enquanto as variáveis forem menores que a entrada, elas se somarão entre sí, adicionando cada valor à lista
+    for(var i=0; i<=number; i+=j){
+        if(i<=number){
+            fibonacci.push(i);
+        };
+        j += i;
+        if(j<=number){
+            fibonacci.push(j);
+        };
+    };
+
+    //Escreve na página os números de Fibonacci
+    document.getElementById("fibonacci").innerHTML = `<h2>${fibonacci}</h2>`;
+};
---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 5/script.js
@@ -0,0 +1,36 @@
+﻿function prime(){
+    //Pega os valores das entradas como números
+    var number1 = Number(document.getElementById("value1").value);
+    var number2 = Number(document.getElementById("value2").value);
+    //Cria uma lista
+    var primes = [];
+
+    //Verifica se o valor de entrada são inteiros, se não, alerta o usuário
+    if(Number.isInteger(number1) && Number.isInteger(number2)){
+
+        //Permite que o usuário escreva corretamente o valor numérico nas entradas
+        if(number1 < number2){
+            var n1 = number1;
+            var n2 = number2;
+        }else{
+            n1 = number2;
+            n2 = number1;
+        };
+
+        //Verifica número por número entre os valores solicitados, e adiciona na lista quando acha um primo
+        for(var i=n1; i<=n2; i++){
+            var prime = true;
+            for(var j=2; j<i; j++){
+                if(i % j == 0){
+                    prime = false;
+                    break;
+                };
+            };
+            if(prime && i>1) primes.push(i);
+        };
+
+        //Escreve na página os números primos
+        document.getElementById("primes").innerHTML = `<h2>${primes}<h2/>`;
+
+    }else alert("Apenas valores inteiros");
+}

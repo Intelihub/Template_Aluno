e6ce1681216cd5e6d5b71f27e25d1136be52bbb2
IgorSFG <igor.garcia@sou.inteli.edu.br>
2022-06-21 10:22:58 -0300

    backend heroku

--- 03_AUT_EST_ENTREGA/Semana 8/app.js
+++ 03_AUT_EST_ENTREGA/Semana 8/app.js
@@ -6,7 +6,7 @@ const hostname = '127.0.0.1';
 const port = 3061;
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const DBPATH = 'dbIgor.db';
+const DBPATH = 'backend/dbIgor.db';
 
 app.use(express.static("../frontend/"));
 
